530536ced3cb4646344eec7f692a490a9cfe1630
cnsusanto75 <cnsusanto75@gmail.com>
2025-10-23 18:03:07 -0400

    write new league saves and move for organization

--- algorithm/league_info.py
+++ algorithm/league_info.py
@@ -1,6 +1,7 @@
 from espn_api.basketball import League
 import json
 
+target_folder = "algorithm/league_data/"
 def get_user_team(league):
     for team in league.teams:
         print(f"Team Name: {team.team_name}, Team ID: {team.team_id}")
@@ -12,7 +13,8 @@ def initialize_league_info():
     s2 = input("Enter your espn_s2 cookie value: ") 
     swid = input("Enter your swid cookie value (including curly braces): ")
     try:
-        team_id = get_user_team(League(league_id=league_id, year=year, espn_s2=s2, swid=swid))
+        league = League(league_id=league_id, year=year, espn_s2=s2, swid=swid)
+        team_id = get_user_team(league)
         league_info = {
             "league_id": league_id,
             "year": year,
@@ -20,9 +22,9 @@ def initialize_league_info():
             "swid": swid,
             "user_team_id": team_id
         }
-        with open("league_info.json", "w") as f:
+        new_file = f"{target_folder}league_{league_id}_{year}_{team_id}.json"
+        with open(new_file, "w") as f:
             json.dump(league_info, f, indent = 4)
-        return League(league_id=league_id, year=year, espn_s2 = s2, swid = swid)
     except Exception as e:
         print(f"An error occurred while fetching league info: {e}")
         return None

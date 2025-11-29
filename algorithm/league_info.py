eaccc861ddcd4e40b727bc63f05946ea9fb2e0d3
cnsusanto75 <cnsusanto75@gmail.com>
2025-11-29 01:25:07 -0500

    Add find best available players

--- algorithm/league_info.py
+++ algorithm/league_info.py
@@ -18,7 +18,27 @@ def get_league_teams(league_id, year, s2, swid):
         teams[team.team_id] = team.team_name
     return teams
 
+
+def get_team_roster(league, user_team_id):
+    roster = []
+    for player in league.teams[user_team_id].roster:
+        roster.append(player.name)
+    return roster
+
+def get_top_free_agents(league, position = None):
+    if position is None:
+        free_agents = league.free_agents(size = 10)
+    else:
+        free_agents = league.free_agents(position = position, size = 10)
+    free_agents_list = []
+    for agent in free_agents:
+        free_agents_list.append(agent.name)
+    return free_agents_list
+
+def fetch_league(league_id, year, s2, swid):
+    league = League(league_id=league_id, year=year, espn_s2=s2, swid=swid)
+    return league
+
 # Only call initialize_league_info when parameters are provided
 if __name__ == "__main__":
-    # Example usage or testing
     pass
